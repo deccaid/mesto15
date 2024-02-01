e7c15eb68f3039361e7c671cc6715cd83c8541b7
decaid <rud.333@mail.ru>
2024-02-01 20:53:51 +0300

    mesto2

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFounderError = require('./errors/NotFoundError');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
