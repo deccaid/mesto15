bb873395bc02d353d7a05d97768dc28ed26f2235
decaid <rud.333@mail.ru>
2024-02-02 13:59:08 +0300

    mesto

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="footer">
-        <p className="footer__author">ⓒ2023 Russia Mesto</p>
+        <p className="footer__author">ⓒ2024 Russia Mesto</p>
       </footer>
     )
 }
